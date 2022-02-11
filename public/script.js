e905dba8dac5738c2ac31faf285765b9444583df
Henry Hiles <henry@henryhiles.com>
2022-02-11 10:23:00 -0500

    Mobile fixes

--- public/script.js
+++ public/script.js
@@ -11,25 +11,11 @@ const dragElement = (element) => {
     let startX, startY, startWidth, startHeight
 
     const doDrag = (event) =>
-        (element.style.width = startWidth + event.clientX - startX + "px")
-
-    const initDrag = (event) => {
-        startX = event.clientX
-        startY = event.clientY
-        startWidth = element.clientWidth
-        startHeight = element.clientHeight
-
-        document.addEventListener("mousemove", doDrag)
-        document.addEventListener("mouseup", () => {
-            document.removeEventListener("mousemove", doDrag)
-            document.removeEventListener("mouseup", this)
-        })
-    }
-
-    const resizer = document.createElement("div")
-    resizer.className = "resizer"
-    element.appendChild(resizer)
-    resizer.addEventListener("mousedown", initDrag)
+        (element.style.width =
+            startWidth +
+            (event.clientX || event.touches[0].clientX) -
+            startX +
+            "px")
 
     let pos1 = 0,
         pos2 = 0,
@@ -37,28 +23,52 @@ const dragElement = (element) => {
         pos4 = 0
 
     const elementDrag = (event) => {
-        pos1 = pos3 - event.clientX
-        pos2 = pos4 - event.clientY
-        pos3 = event.clientX
-        pos4 = event.clientY
+        pos1 = pos3 - (event.clientX || event.touches[0].clientX)
+        pos2 = pos4 - (event.clientY || event.touches[0].clientY)
+        pos3 = event.clientX || event.touches[0].clientX
+        pos4 = event.clientY || event.touches[0].clientY
         element.style.top = `${element.offsetTop - pos2}px`
         element.style.left = `${element.offsetLeft - pos1}px`
-        event.preventDefault()
     }
 
+    const removeFunctions = (resize) => {
+        document.removeEventListener("mousemove", resize ? doDrag : elementDrag)
+        document.removeEventListener("mouseup", this)
+        document.removeEventListener("touchmove", resize ? doDrag : elementDrag)
+        document.removeEventListener("touchend", this)
+    }
+
+    const initDrag = (event) => {
+        startX = event.clientX || event.touches[0].clientX
+        startY = event.clientY || event.touches[0].clientY
+        startWidth = element.clientWidth
+        startHeight = element.clientHeight
+
+        document.addEventListener("mousemove", doDrag)
+        document.addEventListener("touchmove", doDrag)
+        document.addEventListener("touchend", () => removeFunctions(true))
+        document.addEventListener("mouseup", () => removeFunctions(true))
+    }
+
+    const resizer = document.createElement("div")
+    resizer.className = "resizer"
+    element.appendChild(resizer)
+    resizer.addEventListener("mousedown", initDrag)
+    resizer.addEventListener("touchstart", initDrag)
+
     const dragMouseDown = (event) => {
-        if (event.target == resizer) return false
-        pos3 = event.clientX
-        pos4 = event.clientY
+        if (event.target == resizer) return
+
+        pos3 = event.clientX || event.touches[0].clientX
+        pos4 = event.clientY || event.touches[0].clientY
         document.addEventListener("mousemove", elementDrag)
-        document.addEventListener("mouseup", () => {
-            document.removeEventListener("mouseup", this)
-            document.removeEventListener("mousemove", elementDrag)
-        })
-        event.preventDefault()
+        document.addEventListener("touchmove", elementDrag)
+        document.addEventListener("touchend", () => removeFunctions())
+        document.addEventListener("mouseup", () => removeFunctions())
     }
 
     element.addEventListener("mousedown", dragMouseDown)
+    element.addEventListener("touchstart", dragMouseDown)
 }
 
 const connectToNewUser = (userId, stream) => {
